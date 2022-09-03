8eed265f01f3b0acbdbb981437ee298460c64182
Diego Viana <diegovianaf7@gmail.com>
2022-09-03 16:32:25 -0300

    create loops

--- index.js
+++ index.js
@@ -17,7 +17,21 @@ app.get('/', (req, res) => {
 
 app.get('/posts', (req, res) => {
   res.render('posts', {
-    title: 'Express | Posts'
+    title: 'Express | Posts',
+    posts: [
+      {
+        title: 'Tech News',
+        content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil velit impedit dolorum molestiae officia rem adipisci suscipit ea assumenda ullam! Exercitationem fugit molestiae eligendi commodi velit doloribus! Tempore, nostrum ea.'
+      },
+      {
+        title: 'Creating a Server with Node.js',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde velit odio neque, debitis possimus voluptatum quo sequi aperiam harum repellat laborum pariatur reprehenderit accusamus.'
+      },
+      {
+        title: 'How to Become a Frontend Developer?',
+        content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, sapiente. Ullam neque aliquam dolor libero officiis consectetur corporis in natus atque cumque voluptate asperiores, debitis ex, incidunt quas.'
+      }
+    ]
   })
 })
 
