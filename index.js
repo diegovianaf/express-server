5cdad9ac3008987bd5ab06e090dbf7dae891a23f
Diego Viana <diegovianaf7@gmail.com>
2022-09-03 16:09:59 -0300

    add title according to the page

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Express | Home'
+  })
 })
 
 app.get('/posts', (req, res) => {
-  res.render('posts')
+  res.render('posts', {
+    title: 'Express | Posts'
+  })
 })
 
 app.use((req, res) => {
